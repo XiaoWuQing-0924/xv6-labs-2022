f43c019106c0d2cc67b59bb76855e0c5f3be252b
Jxl <1620196583@qq.com>
2023-02-15 14:27:24 +0800

    last

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -66,6 +66,7 @@ kfree(void *pa)
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
+// 返回的三真实的物理地址
 void *
 kalloc(void)
 {
